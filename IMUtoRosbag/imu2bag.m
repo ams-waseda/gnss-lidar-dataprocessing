58adc0bf2610f126e52762afcc041ca5c049eb2f
Taro <taroz1461@gmail.com>
2025-08-05 23:14:54 +0900

    IMU timestamp bug fix, test script added

--- IMUtoRosbag/imu2bag.m
+++ IMUtoRosbag/imu2bag.m
@@ -28,7 +28,7 @@ assert(all(diff(tgyro.t)==seconds(0.005)));
 assert(all(tgyro.t==tacc.t));
 
 % Plot
-tiledlayout(2,1,"TileSpacing","compact");
+tiledlayout(3,1,"TileSpacing","compact");
 nexttile;
 plot(acc); grid on;
 title("Acceleration");
@@ -39,6 +39,12 @@ plot(gyro); grid on;
 title("Angular rate");
 ylabel("deg/s");
 legend("x","y","z");
+nexttile;
+plot(diff(unix_time));
+grid on;
+ylim([0 0.01]);
+title("Time difference");
+ylabel("s");
 
 %% Convert to ROS bag
 bagWriter = ros2bagwriter("../outputs/"+dirname+"/imu","StorageFormat","mcap");
@@ -46,8 +52,9 @@ for i=1:1:length(unix_time)
     message2 = ros2message("sensor_msgs/Imu");
 
     % Header/Stamp
-    message2.header.stamp.sec = int32(unix_time(i));
+    message2.header.stamp.sec = int32(fix(unix_time(i)));
     message2.header.stamp.nanosec = uint32((unix_time(i)-fix(unix_time(i)))*10^9);
+    message2.header.frame_id = 'imu';
 
     % Angular rate
     message2.angular_velocity.x = deg2rad(gyro(i,1)); % rad/s
@@ -59,6 +66,6 @@ for i=1:1:length(unix_time)
     message2.linear_acceleration.y = acc(i,2);
     message2.linear_acceleration.z = acc(i,3);
 
-    write(bagWriter, "/imu", unix_time(i), message2);
+    write(bagWriter, "/imu", message2.header.stamp, message2);
 end
 delete(bagWriter);
